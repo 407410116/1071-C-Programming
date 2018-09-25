834efcc1a34a74d1fd4134c8d09d5558092bee02
407410116 <43335270+407410116@users.noreply.github.com>
2018-09-26 00:04:37 +0800

    Add files via upload

--- w03/BMI using while.cpp
+++ w03/BMI using while.cpp
@@ -1,1 +1,42 @@
-
+#include<stdio.h>
+int main()
+{
+    double weight,height,BMI;
+	double m;
+	
+	printf("Enter weight(kg) and height(cm):");
+	scanf("%lf %lf",&weight,&height);
+	m=height/100;
+	BMI=weight/(m*m);
+	while(weight>0&&height>0)
+	{
+		if(BMI<18.5)  
+	    {
+        printf("BMI:%lf,體重過輕\n",BMI);
+        }
+		else if(BMI>=18.5&&BMI<24)
+		{
+			printf("BMI:%lf,正常範圍\n",BMI);
+		}
+		else if(BMI>=24&&BMI<27)
+		{
+			printf("BMI:%lf,過重\n",BMI); 
+		}
+		else if(BMI>=27&&BMI<30)
+		{
+			printf("BMI:%lf,輕度肥胖\n",BMI); 
+		}
+		else if(BMI>=30&&BMI<35)
+		{
+			printf("BMI:%lf,中度肥胖\n",BMI); 
+		}
+		else if(BMI>=35)
+		{
+			printf("BMI:%lf,重度肥胖\n",BMI); 
+		}
+		printf("Enter weight(kg) and height(cm):");
+	    scanf("%lf %lf",&weight,&height);
+	    m=height/100;
+	    BMI=weight/(m*m);
+	}
+}
