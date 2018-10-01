fa8f4bc2cac162a0e952ce50e5089b31f6eaa4df
407410116 <43335270+407410116@users.noreply.github.com>
2018-10-01 22:41:54 +0800

    Update menu.cpp

--- w04/menu.cpp
+++ w04/menu.cpp
@@ -18,13 +18,13 @@ int main()
         switch(choice)
         {
             case 1:
-              printf("Enter height :");
-              scanf("%d",&height);
+              printf("Enter height and width :");
+              scanf("%d %d",&height,&width);
               for(int i=1;i<=height;i+=1)
               {
                   for(int j=1;j<=width;j+=1)
                   {
-                      if(i==1 || i==height || i==j)
+                      if(i==1 || i==height || j==1 || j==width)
                       {
                          printf("*");
                       }
@@ -43,6 +43,66 @@ int main()
               switch(VerticalTriangle)
               {
                case 1:
+                  printf("Enter height:");
+                  scanf("%d",&height);
+                  for(int i=1;i<=height;i+=1)
+                  {
+                      for(int j=1;j<=height;j+=1)
+                      {
+                          if(j==1 || i==height || i==j)
+                          {
+                             printf("*");
+                          }
+                          else
+                          {
+                             printf(" ");
+                          }
+                      }
+                      printf("\n");
+                  }
+                  break;
+
+                  case 2:
+                  printf("Enter height:");
+                  scanf("%d",&height);
+                  for(int i=1;i<=height;i+=1)
+                  {
+                      for(int j=height;j>0;j-=1)
+                      {
+                          if(j==1 || i==height || i==j)
+                          {
+                             printf("*");
+                          }
+                          else
+                          {
+                             printf(" ");
+                          }
+                      }
+                      printf("\n");
+                  }
+                  break;
+
+                  case 3:
+                  printf("Enter height:");
+                  scanf("%d",&height);
+                  for(int i=1;i<=height;i+=1)
+                  {
+                      for(int j=height;j>0;j-=1)
+                      {
+                          if(j==height || i==1 || i==j)
+                          {
+                             printf("*");
+                          }
+                          else
+                          {
+                             printf(" ");
+                          }
+                      }
+                      printf("\n");
+                  }
+                  break;
+
+                  case 4:
                   printf("Enter height:");
                   scanf("%d",&height);
                   for(int i=1;i<=height;i+=1)
@@ -72,3 +132,5 @@ int main()
 
 return 0;
 }
+
+
