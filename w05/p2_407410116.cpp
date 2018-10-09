8f273cc7fd5c3ca7675969c4f10c78fb5601175b
407410116 <43335270+407410116@users.noreply.github.com>
2018-10-09 18:49:58 +0800

    Create p2_407410116.cpp

---
+++ w05/p2_407410116.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int height1,height2;
+    float kg1,kg2;
+    float m1,m2;
+    
+    printf("Enter height  (-1 to exit): ");
+    scanf("%d %d",height1 ,height2);
+    
+    kg1=
+    
+    return 0;
+}
