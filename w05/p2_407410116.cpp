424e91c7df7dfea5edf79d5eab344e57b6b560a6
407410116 <43335270+407410116@users.noreply.github.com>
2018-10-10 11:35:28 +0800

    Update p2_407410116.cpp

---
+++ w05/p2_407410116.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int cm1,cm2,cm;
+    float kg1,kg2,BMI;
+    while(1)
+    {
+        printf("Enter height (-1 to exit): ");
+        scanf("%d %d",&cm1 ,&cm2);
+        if(cm1==-1 && cm2==-1) return 0;
+
+        for(int i=cm1;cm2>=i;i++)
+        {
+            kg1=(i/100.)*(i/100.)*18.5;
+            kg2=(i/100.)*(i/100.)*24;
+            printf(" %d cm: %.1f ~ %.1f (kg)\n",i ,kg1 ,kg2);
+        }
+        printf("\n");
+    }
+    return 0;
+}
